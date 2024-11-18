21f5f850e01137d4e35b0980807929b928399691
karunakargoud <karunakargoud1116@gmail.com>
2024-11-18 22:16:32 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arr = input.Split(' ');
+            string rev = "";
+            foreach (string word in arr)
+            {
+                string reverseWord = "";
+                for (int i = 0; i < word.Length; i++)
+                {
+                    reverseWord += word[word.Length - 1 - i];
+
+                }
+                rev += reverseWord + " ";
+            }
+            Console.WriteLine(rev);
+        }
+    }
+}
